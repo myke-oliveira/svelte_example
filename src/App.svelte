89601c7c7268a5542f6eb7f5d79f01030f60d1b4
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2022-08-28 20:10:21 -0300

    👍 DONE: Binomial Formula

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,13 @@
 <script lang="ts">
-import { onMount } from "svelte";
-  let n = 5;
+  import Equation from "./Equation.svelte";
 
+  let n = 2;
   $: equation = binomialEquation(n);
   $: {
     if (n < 0) {
       alert('O expoente deve ser maior ou igual a zero 0');
       n = 0;
     }
-    MathJax.Hub.Typeset(document.getElementById('equation'), () => {yarn})
   }
 
   function binomialEquation(n: number) {
@@ -21,7 +20,7 @@ import { onMount } from "svelte";
       terms.push(term)
     }
   
-    return `$$ (x+y)^{${n}} = ${terms.join(' + ')} $$`
+    return `$$ (x+y)^{${n}} = ${terms.join(' + ')} $$`;
   }
 
   function binomialCoefficient(n: number, k: number) {
@@ -37,29 +36,14 @@ import { onMount } from "svelte";
     return f
   }
 
-  onMount(() => {
-    let script = document.createElement('script');
-    script.src = "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js";
-    document.head.append(script);
-
-    script.onload = () => {
-      MathJax = {
-        tex: {inlineMath: [['$', '$'], ['\\(', '\\)']]},
-        svg: {fontCache: 'global'}
-      };
-    };
-  });
-
 </script>
 
 <main>
   <form>
-    <label for="exponent">exponent</label>
-    <input type="number" name="exponent" id="exponent" bind:value={n} />
+    <label for="exponent">Expoente: </label>
+    <input type="number" name="exponent" id="exponent" bind:value={n}/>
   </form>
-  <div id="equation">
-    {@html equation}
-  </div>
+  <Equation {equation}/>  
 </main>
 
 <style>
---
+++ src/Equation.svelte
@@ -0,0 +1,12 @@
+<script>
+  const MathJax = window.MathJax;
+  export let equation;
+
+  $: equation && MathJax.typesetPromise().then(() => {
+    document.getElementById('equation').innerHTML = equation
+    MathJax.typesetPromise();
+  });
+</script>
+
+<div id="equation">
+</div>
